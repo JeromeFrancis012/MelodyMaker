f5c5e96850ce7fb853f98f3ec99049cd07ac00cf
JeromeFrancis012 <74269168+JeromeFrancis012@users.noreply.github.com>
2021-01-15 13:08:21 -0600

    Update notes.pl

--- notes.pl
+++ notes.pl
@@ -6,3 +6,11 @@ chord(4, 6, 1, e).
 chord(5, 0, 2, f).
 chord(6, 1, 3, g).
 
+match(A, X) :- chord(X,Y,Z,A).
+match(A, Y) :- chord(X,Y,Z,A).
+match(A, Z) :- chord(X,Y,Z,A).
+
+melody([A], [X]) :- match(A,X).
+
+melody([H1|L], [H2|M]) :- match(H1,H2), melody(L,M).
+
